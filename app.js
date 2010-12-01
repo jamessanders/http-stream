a180857b242fdba5fb717edecbf4f0700ee58f06
James Sanders <james.sanders@thisismedium.com>
2010-11-30 20:38:58 -0500

    Added test and a little more meat

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var events = require("events");
+var events  = require("events");
+var http    = require("http");
+var fs      = require("fs");
+var cookies = require("./lib/cookie-node");
 
 function HttpStream (id, response) {
 
-  var is_ready = response ? true : false;
+  var is_ready = response != undefined ? true : false;
   var wait_buffer = [];
-  var wait_callbacks = [];
+  var self = this;
 
   this.getId = function () {
     return id;
@@ -17,8 +20,7 @@ function HttpStream (id, response) {
   };
 
   this.write = function (data, callback) {
-    wait_buffer.push(data);
-    wait_callbacks.push(callback);
+    wait_buffer.push([data, callback]);
     return this;
   };
 
@@ -27,26 +29,71 @@ function HttpStream (id, response) {
   };
 
   function tick() {
-    if (this.isReady() && wait_buffer.length > 0) {
-      response.writeHead(200);
-      response.end(wait_buffer.join(""));
-      wait_buffer = [];
+    if (self.isReady() && wait_buffer.length > 0) {
+      var packet = wait_buffer.pop();
+      response.writeHead(200,{"Content-Type": "application/octet-stream"});
+      response.end(packet[0]);
       is_ready = false;
-      for (var i = 0; i < wait_callbacks.length; i++) {
-        wait_callbacks.pop()();
-      }
+      if (packet[1]) packet[1]();
     }
     process.nextTick(tick);
   };
   tick();
-
 }
+HttpStream.prototype = events.EventEmitter.prototype
 
 function HttpStreamServer (httpServer) {
 
-  var current_id = 1;
+  var current_id = 0;
   var streams = {};
+  var self = this;
+  var server_id = Math.floor(Math.random() * 10000);
   
-  this.
+  function getNewId () {
+    return ++current_id;
+  };
+
+  httpServer.on("request", function (req, res) {
+    if (req.url == "/stream") {
+      var session_id = req.getCookie("stream"+server_id);
+      if (session_id && streams[session_id]) {
+        console.log("Found stream: ");
+        console.log(streams[session_id].getId());
+        streams[session_id].updateResponseObject(res);
+      } else {
+        var new_id = getNewId();
+        res.setCookie("stream"+server_id, new_id);
+        var stream = new HttpStream(new_id);
+        streams[current_id] = stream;
+        res.writeHead(200,{});
+        res.end("");
+        process.nextTick(function() { self.emit("connection", stream); });
+      }
+    } else {
+      fs.readFile("./test.html", function(err, data) {
+        res.writeHead(200,{"Content-Type": "text/html"});
+        res.end(data);
+      });
+    }
+  });
 
 }
+HttpStreamServer.prototype = events.EventEmitter.prototype
+
+////////////////////////////////////////////////////////////////////////
+// test app
+////////////////////////////////////////////////////////////////////////
+
+var http_server = http.createServer();
+var stream_server = new HttpStreamServer(http_server);
+
+stream_server.on("connection", function(stream) {
+  console.log("Got connection for stream #"+stream.getId());
+  var c = 0;
+  setInterval(function() { 
+    console.log("Writing to stream #" + stream.getId());
+    stream.write(stream.getId() + " Hello world! " + (c++));
+  }, 100);
+});
+
+http_server.listen(8080, "0.0.0.0");
